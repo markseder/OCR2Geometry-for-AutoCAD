2e9da7250536f033e7d4d3d9905a8c57b6038890
Pavel Matveev <pavelmatveev84@gmail.com>
2026-09-17 11:01:09 +1100

    Add Z coordinate to point model

--- src/OCR2Geometry/Models/CoordinatePoint.cs
+++ src/OCR2Geometry/Models/CoordinatePoint.cs
@@ -5,12 +5,14 @@ namespace OCR2Geometry.Models
         public int Number { get; set; }
         public double X { get; set; }
         public double Y { get; set; }
+        public double Z { get; set; }
 
-        public CoordinatePoint(int number, double x, double y)
+        public CoordinatePoint(int number, double x, double y, double z = 0.0)
         {
             Number = number;
             X = x;
             Y = y;
+            Z = z;
         }
     }
 }
